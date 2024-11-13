3e706edc176e37ea00c65effe7ab44a3111d5373
Hasnain-MarkCoders <hasnain.raza@markcoders.com>
2024-11-13 19:19:54 +0500

    base url updated

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = "http://localhost:4567/api/v1";
 
-export const API_BASE_URL = "https://aws.markcoders.com/localvolts/api/v1";
+// export const API_BASE_URL = "https://aws.markcoders.com/localvolts/api/v1";
+export const API_BASE_URL = "https://anton.markcoders.com/localvolts/api/v1";
